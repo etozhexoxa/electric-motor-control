e8b6017ebb19ffb888b38808b7561d6814c8cc33
Timofey <ttolstov777@gmail.com>
2022-07-13 00:33:42 +0700

    project-publication

---
+++ Praktika/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace Praktika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbState = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCurrent = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbState
+            // 
+            this.tbState.Location = new System.Drawing.Point(127, 6);
+            this.tbState.Name = "tbState";
+            this.tbState.ReadOnly = true;
+            this.tbState.Size = new System.Drawing.Size(184, 20);
+            this.tbState.TabIndex = 0;
+            this.tbState.Text = "ВЫКЛ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Состояние прибора:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Скорость вращения:";
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.Location = new System.Drawing.Point(127, 32);
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.ReadOnly = true;
+            this.tbSpeed.Size = new System.Drawing.Size(184, 20);
+            this.tbSpeed.TabIndex = 2;
+            this.tbSpeed.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Значение тока:";
+            // 
+            // tbCurrent
+            // 
+            this.tbCurrent.Location = new System.Drawing.Point(127, 58);
+            this.tbCurrent.Name = "tbCurrent";
+            this.tbCurrent.ReadOnly = true;
+            this.tbCurrent.Size = new System.Drawing.Size(184, 20);
+            this.tbCurrent.TabIndex = 4;
+            this.tbCurrent.Text = "0";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(326, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(646, 199);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Дата: c";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Время: c";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(324, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Состояние базы данных:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(463, 208);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "отключена";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(92, 130);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(81, 20);
+            this.dateTimePicker1.TabIndex = 28;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(92, 156);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(81, 20);
+            this.dateTimePicker2.TabIndex = 29;
+            this.dateTimePicker2.Value = new System.DateTime(2021, 7, 7, 18, 36, 0, 0);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(179, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "по";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(179, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "по";
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker3.Location = new System.Drawing.Point(204, 129);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(81, 20);
+            this.dateTimePicker3.TabIndex = 32;
+            // 
+            // dateTimePicker4
+            // 
+            this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker4.Location = new System.Drawing.Point(204, 156);
+            this.dateTimePicker4.Name = "dateTimePicker4";
+            this.dateTimePicker4.ShowUpDown = true;
+            this.dateTimePicker4.Size = new System.Drawing.Size(81, 20);
+            this.dateTimePicker4.TabIndex = 33;
+            this.dateTimePicker4.Value = new System.DateTime(2021, 7, 7, 18, 36, 0, 0);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(291, 132);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 34;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(291, 159);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 35;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 23);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Применить фильтр";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 230);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbCurrent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbState);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Контроль и учёт работы электродвигателя";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbState;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCurrent;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Praktika/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO.Ports;
+using System.IO;
+using ThreadTimer = System.Threading.Timer;
+
+namespace Praktika
+{
+    public partial class Form1 : Form
+    {
+
+        // Инициализация компонентов формы.
+        public Form1()
+        {
+            InitializeComponent();
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+        }
+
+        public string _db = "server=localhost;database=db;username=root;password=;charset=utf8mb4;";
+        public string _select = "SELECT * FROM praktika_table"; 
+        public string _insert = "INSERT INTO praktika_table (date, time, state, speed, current) VALUES (CURDATE(), CURTIME(), @state, @speed, @current)";
+        public string _date, _time, _state;
+        public int _speed, _current;
+        public MySqlConnection _connection; // Для подключения к БД.
+        public MySqlDataAdapter adapter_all, adapter_filter; // Для запросов вывода данных из таблицы.
+        public MySqlCommand _command; // Ввод данных в таблицу.
+        public DataTable _table; // Для DGV-таблицы.
+        public SerialPort _port; // Для COM-порта.
+        public DateTime _datetime; // Для даты и времени
+        public ThreadTimer timer1;
+
+
+        // Инициализация при запуске приложения
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Проверка работоспособности порта.
+            _connection = new MySqlConnection(_db);
+            _command = new MySqlCommand(_insert, _connection);
+            try
+            {
+                _connection.Open();
+                adapter_all = new MySqlDataAdapter(_select, _connection);
+                _table = new DataTable();
+                label10.Text = "подключена";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения базы данных: " + Environment.NewLine + ex.Message);
+            }
+
+            try
+            {
+                _port = new SerialPort("COM7", 38400);
+                _port.DataBits = 8;
+                _port.Parity = Parity.None;
+                _port.StopBits = StopBits.One;
+                _port.Open();   // Тестовое открытие
+                //timer1 = new ThreadTimer(timer1_Tick, null, 0, 1000); // Если успешно - запуск таймера.
+                timer1 = new ThreadTimer(timer1_Tick, null, 1000, 1000); // Если успешно - запуск таймера.
+            }
+            catch
+            {   // Не удалось подключиться к COM-порту - таймер не запускается,
+                // вывод сообщения о том, что подключение не установлено.
+                MessageBox.Show("COM-порт не подключен! " +
+                    "Доступна только работа с базой данных. " +
+                    "Проверьте соединение и перезапустите приложение!");
+            }
+            finally
+            {
+                if (_port.IsOpen)
+                    _port.Close();  // и закрытие порта.
+            }
+        }
+
+         // Обработка событий таймера на вывод БД
+        // во время выполнения приложения.
+        public void timer1_Tick(Object state)
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _port.Open();
+                byte[] message = new byte[] { 0x01, 0x03, 0x21, 0x01, 0x00, 0x04 };
+                byte[] crc = ModbusCRC16Calc(message);
+                byte[] request = new byte[message.Length + crc.Length];
+                message.CopyTo(request, 0);
+                crc.CopyTo(request, request.Length - 2);
+                _port.Write(request, 0, request.Length);
+                byte[] response = ReadExact(_port.BaseStream, 13);
+                _port.Close();
+                int vfdState = MakeWord(response[3], response[4]);
+                string stateString;
+                if ((vfdState & 3) == 3)
+                    stateString = "ON";
+                else
+                {
+                    stateString = "OFF";
+                }
+                int speed = MakeWord(response[7], response[8]);
+                int current = MakeWord(response[9], response[10]);
+
+                Action a = () =>
+                {
+                    tbState.Text = stateString;
+                    tbSpeed.Text = speed.ToString();
+                    tbCurrent.Text = current.ToString();
+                };
+                this.Invoke(a);
+
+                _datetime = DateTime.Now;
+                _command.Parameters.Clear();
+                _command.CommandText = _insert;
+                _command.Parameters.AddWithValue("state", stateString);
+                _command.Parameters.AddWithValue("speed", speed);
+                _command.Parameters.AddWithValue("current", current);
+                try
+                {
+                    _command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось добавить данные! " + ex.Message);
+                }
+                
+            }
+            else
+            {
+                label10.Text = "отключена";
+                MessageBox.Show("Подключение к базе данных прервано!" +
+                    "Перезапустите программу!");
+                timer1.Change(0, System.Threading.Timeout.Infinite);
+            }
+        }
+
+        private int MakeWord(byte HB, byte LB)
+        {
+            return ((HB << 8) | LB);
+        }
+
+        // Обработка нажатия кнопки "Применить фильтр".
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                string s_date_1 = "'" + dateTimePicker1.Text + "'";
+                string s_date_2 = "'" + dateTimePicker3.Text + "'";
+                string s_time_1 = "'" + dateTimePicker2.Text + "'";
+                string s_time_2 = "'" + dateTimePicker4.Text + "'";
+
+                string search = _select + " WHERE ";
+
+                if ((checkBox1.Checked == true) || (checkBox2.Checked == true))
+                {
+                    if ((checkBox1.Checked == true)) { search = search + $"(date BETWEEN '{dateTimePicker1.Value.Date.ToString("yyyy-MM-dd")}' AND '{dateTimePicker3.Value.Date.ToString("yyyy-MM-dd")}')"; }
+                    if ((checkBox1.Checked == true) && (checkBox2.Checked == true)) { search = search + " AND "; }
+                    if ((checkBox2.Checked == true)) { search = search + $"(time BETWEEN '{dateTimePicker2.Value.TimeOfDay.ToString()}' AND '{dateTimePicker4.Value.TimeOfDay.ToString()}')"; }
+                    search += ";";
+
+                    adapter_filter = new MySqlDataAdapter(search, _connection);
+                    _table.Clear();
+                    adapter_filter.Fill(_table);
+                    dataGridView1.DataSource = _table;
+                }
+                else { MessageBox.Show("Поставьте галочки напротив фильтров, которые необходимо применить!"); }
+            }
+            else
+            {
+                timer1.Dispose();
+                label10.Text = "отключена";
+                MessageBox.Show("Подключение к базе данных прервано!" +
+                    "Перезапустите программу!");
+            }
+        }
+
+        // Выключаем таймер, закрываем соединение с БД
+        // после закрытия приложения.
+        private void OnApplicationExit(Object sender, EventArgs e)
+        {
+            timer1.Dispose();
+            _connection.Close();
+        }
+    
+        byte[] ReadExact(Stream s, int nbytes)
+        {
+            var buf = new byte[nbytes];
+            var readpos = 0;
+            while (readpos < nbytes)
+            {
+                readpos += s.Read(buf, readpos, nbytes - readpos);
+            }
+            return buf;
+        }
+
+        public static byte[] ModbusCRC16Calc(byte[] Message)
+        {
+            //выдаваемый массив CRC
+            byte[] CRC = new byte[2];
+            ushort Register = 0xFFFF; // создаем регистр, в котором будем сохранять высчитанный CRC
+            ushort Polynom = 0xA001; //Указываем полином, он может быть как 0xA001(старший бит справа), так и его реверс 0x8005(старший бит слева, здесь не рассматривается), при сдвиге вправо используется 0xA001
+
+            for (int i = 0; i < Message.Length; i++) // для каждого байта в принятом\отправляемом сообщении проводим следующие операции(байты сообщения без принятого CRC)
+            {
+                Register = (ushort)(Register ^ Message[i]); // Делим через XOR регистр на выбранный байт сообщения(от младшего к старшему)
+
+                for (int j = 0; j < 8; j++) // для каждого бита в выбранном байте делим полученный регистр на полином
+                {
+                    if ((ushort)(Register & 0x01) == 1) //если старший бит равен 1 то
+                    {
+                        Register = (ushort)(Register >> 1); //сдвигаем на один бит вправо
+                        Register = (ushort)(Register ^ Polynom); //делим регистр на полином по XOR
+                    }
+                    else //если старший бит равен 0 то
+                    {
+                        Register = (ushort)(Register >> 1); // сдвигаем регистр вправо
+                    }
+                }
+            }
+
+            CRC[1] = (byte)(Register >> 8); // присваеваем старший байт полученного регистра младшему байту результата CRC (CRClow)
+            CRC[0] = (byte)(Register & 0x00FF); // присваеваем младший байт полученного регистра старшему байту результата CRC (CRCHi) это условность Modbus — обмен байтов местами.
+
+            return CRC;
+        }
+    }
+}
